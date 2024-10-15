668d4157a3464fa6320887115ae608b725f2ac11
gootibi <gootibi@gmail.com>
2024-10-15 15:15:44 +0200

    Step 3: MongoDB model setup and Express Error handling

---
+++ backend/src/models/note.model.ts
@@ -0,0 +1,15 @@
+import { InferSchemaType, Schema, model } from "mongoose";
+
+const noteSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    text: {
+        type: String,
+    },
+}, { timestamps: true });
+
+export type Note = InferSchemaType<typeof noteSchema>;
+
+export default model<Note>("Note", noteSchema);
--- backend/src/services/expressApp.services.ts
+++ backend/src/services/expressApp.services.ts
@@ -1,10 +1,30 @@
 import "dotenv/config"
-import express, { Application, Request, Response } from "express";
+import express, { Application, NextFunction, Request, Response } from "express";
+import NoteModel from "../models/note.model";
 
 const app: Application = express();
 
-app.get("/", (req: Request, res: Response) => {
-    res.send("Welcome homapage!");
-})
+app.get("/", async (req, res, next) => {
+    try {
+        // throw Error("Something went wrong!");
+        const notes = await NoteModel.find().exec();
+        res.status(200).json(notes);
+    } catch (error) {
+        next(error);
+    }
+});
+
+app.use((req, res, next) => {
+    next(Error("Endpoint Not Found"));
+});
+
+app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
+    console.error(error);
+    let errorMessage = "An unknown error occurred";
+    if (error instanceof Error) {
+        errorMessage = error.message;
+        res.status(500).json({ error: errorMessage });
+    }
+});
 
 export default app;
