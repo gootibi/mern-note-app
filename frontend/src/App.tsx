c5788335b4256151dabeba14369e9ac21f6f3fb8
gootibi <gootibi@gmail.com>
2024-10-17 22:25:56 +0200

    Step 13: Add text or spinner when the states is loading/error/empty

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { Note as NoteModel } from './models/note.model';
 import Note from './components/Note';
-import { Button, Col, Container, Row } from 'react-bootstrap';
+import { Button, Col, Container, Row, Spinner } from 'react-bootstrap';
 import styles from "./styles/NotesPage.module.css"
 import styleUtils from "./styles/utils.module.css"
 import * as NotesApi from "./network/notes_api";
@@ -9,79 +9,100 @@ import AddEditNoteDialog from './components/AddEditNoteDialog';
 import { FaPlus } from "react-icons/fa";
 
 function App() {
-  const [notes, setNotes] = useState<NoteModel[]>([]);
+    const [notes, setNotes] = useState<NoteModel[]>([]);
+    const [notesLoading, setNotesLoading] = useState<boolean>(true);
+    const [showNotesLoadingError, setShowNotesLoadingError] = useState<boolean>(false);
 
-  const [showAddNoteDialog, setShowAddNoteDialog] = useState(false);
-  const [noteToEdit, setNoteToEdit] = useState<NoteModel | null>(null);
+    const [showAddNoteDialog, setShowAddNoteDialog] = useState<boolean>(false);
+    const [noteToEdit, setNoteToEdit] = useState<NoteModel | null>(null);
 
-  useEffect(() => {
-    async function loadNotes() {
-      try {
-        const notes = await NotesApi.fetchNotes();
-        setNotes(notes);
-      } catch (error) {
-        console.log(error);
-        alert(error);
-      }
+    useEffect(() => {
+        async function loadNotes() {
+            try {
+                setShowNotesLoadingError(false);
+                setNotesLoading(true);
+                const notes = await NotesApi.fetchNotes();
+                setNotes(notes);
+            } catch (error) {
+                console.log(error);
+                setShowNotesLoadingError(true);
+            } finally {
+                setNotesLoading(false);
+            };
+        }
+        loadNotes();
+    }, []);
+
+    async function deleteNote(note: NoteModel) {
+        try {
+            await NotesApi.deleteNote(note._id);
+            setNotes(notes.filter(existingNote => existingNote._id !== note._id))
+        } catch (error) {
+            console.error(error);
+            alert(error);
+        }
     }
-    loadNotes();
-  }, []);
 
-  async function deleteNote(note: NoteModel) {
-    try {
-      await NotesApi.deleteNote(note._id);
-      setNotes(notes.filter(existingNote => existingNote._id !== note._id))
-    } catch (error) {
-      console.error(error);
-      alert(error);
-    }
-  }
+    const notesGrid =
+        <Row xs={1} md={2} lg={4} className={`g-4 ${styles.noteGrid}`}>
+            {notes.map(note => (
+                <Col key={note._id}>
+                    <Note
+                        note={note}
+                        className={styles.note}
+                        onNoteClick={(note) => setNoteToEdit(note)}
+                        onDeleteNoteClick={deleteNote}
+                    />
+                </Col>
+            ))}
+        </Row>
 
-  return (
-    <Container>
-      <Button
-        onClick={() => setShowAddNoteDialog(true)}
-        className={`mb-4 ${styleUtils.blockCenter} ${styleUtils.flexCenter}`}
-      >
-        <FaPlus />
-        Add new note
-      </Button>
-      <Row xs={1} md={2} lg={4} className='g-4'>
-        {notes.map(note => (
-          <Col key={note._id}>
-            <Note
-              note={note}
-              className={styles.note}
-              onNoteClick={(note) => setNoteToEdit(note)}
-              onDeleteNoteClick={deleteNote}
-            />
-          </Col>
-        ))}
-      </Row>
-      {
-        showAddNoteDialog &&
-        <AddEditNoteDialog
-          onDismiss={() => setShowAddNoteDialog(false)}
-          onNoteSaved={(newNote) => {
-            setNotes([...notes, newNote]);
-            setShowAddNoteDialog(false);
-          }}
-        />
-      }
+    return (
+        <Container className={styles.notesPage}>
+            <Button
+                onClick={() => setShowAddNoteDialog(true)}
+                className={`mb-4 ${styleUtils.blockCenter} ${styleUtils.flexCenter}`}
+            >
+                <FaPlus />
+                Add new note
+            </Button>
 
-      {
-        noteToEdit &&
-        <AddEditNoteDialog
-          noteToEdit={noteToEdit}
-          onDismiss={() => setNoteToEdit(null)}
-          onNoteSaved={(updateNote) => {
-            setNotes(notes.map(existingNote => existingNote._id === updateNote._id ? updateNote : existingNote));
-            setNoteToEdit(null);
-          }}
-        />
-      }
-    </Container>
-  );
+            {notesLoading && <Spinner animation='border' variant='primary' />}
+            {showNotesLoadingError && <p>Something went wrong. Please refresh the page.</p>}
+            {!notesLoading && !showNotesLoadingError &&
+                <>
+                    {
+                        notes.length > 0
+                            ? notesGrid
+                            : <p>You don't have any notes yet!</p>
+                    }
+                </>
+            }
+
+            {
+                showAddNoteDialog &&
+                <AddEditNoteDialog
+                    onDismiss={() => setShowAddNoteDialog(false)}
+                    onNoteSaved={(newNote) => {
+                        setNotes([...notes, newNote]);
+                        setShowAddNoteDialog(false);
+                    }}
+                />
+            }
+
+            {
+                noteToEdit &&
+                <AddEditNoteDialog
+                    noteToEdit={noteToEdit}
+                    onDismiss={() => setNoteToEdit(null)}
+                    onNoteSaved={(updateNote) => {
+                        setNotes(notes.map(existingNote => existingNote._id === updateNote._id ? updateNote : existingNote));
+                        setNoteToEdit(null);
+                    }}
+                />
+            }
+        </Container>
+    );
 }
 
 export default App;
