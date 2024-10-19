c7d3da64f232023c09b6972ee4c17fa4e829b695
gootibi <gootibi@gmail.com>
2024-10-19 15:45:06 +0200

    Step 17: Add routing with react-router-dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,15 @@
 import { useEffect, useState } from 'react';
 import { Container } from 'react-bootstrap';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import LoginModal from './components/LoginModal';
 import NavBar from './components/NavBar';
-import NotesPageLoggedInView from './components/NotesPageLoggedInView';
-import NotesPageLoggedOutView from './components/NotesPageLoggedOutView';
 import SignUpModal from './components/SignUpModal';
 import { User } from './models/user.model';
 import * as UserApi from "./network/user_api";
-import styles from "./styles/NotesPage.module.css";
+import NotesPage from './pages/NotesPage';
+import NotFoundPage from './pages/NotFoundPage';
+import PrivacyPage from './pages/PrivacyPage';
+import styles from "./styles/App.module.css";
 
 function App() {
 
@@ -30,44 +32,55 @@ function App() {
     }, []);
 
     return (
-        <div>
-            <NavBar
-                loggedInUser={loggedInUser}
-                onLoginClicked={() => setShowLoginModal(true)}
-                onSignUpClicked={() => setShowSignUpModal(true)}
-                onLogoutSuccess={() => setLoggedInUser(null)}
-            />
-            <Container className={styles.notesPage}>
-                <>
-                    {
-                        loggedInUser
-                            ? <NotesPageLoggedInView />
-                            : <NotesPageLoggedOutView />
-                    }
-                </>
-            </Container>
-            {
-                showSignUpModal &&
-                <SignUpModal
-                    onDismiss={() => setShowSignUpModal(false)}
-                    onSignUpSuccessful={(user) => {
-                        setLoggedInUser(user);
-                        setShowSignUpModal(false);
-                    }}
+        <BrowserRouter>
+            <div>
+                <NavBar
+                    loggedInUser={loggedInUser}
+                    onLoginClicked={() => setShowLoginModal(true)}
+                    onSignUpClicked={() => setShowSignUpModal(true)}
+                    onLogoutSuccess={() => setLoggedInUser(null)}
                 />
-            }
 
-            {
-                showLoginModal &&
-                <LoginModal
-                    onDismiss={() => setShowLoginModal(false)}
-                    onLoginSuccess={(user) => {
-                        setLoggedInUser(user);
-                        setShowLoginModal(false);
-                    }}
-                />
-            }
-        </div>
+                <Container className={`${styles.pageContainer}`}>
+                    <Routes>
+                        <Route
+                            path='/'
+                            element={<NotesPage loggedInUser={loggedInUser} />}
+                        />
+                        <Route
+                            path='/privacy'
+                            element={<PrivacyPage />}
+                        />
+                        <Route
+                            path='/*'
+                            element={<NotFoundPage />}
+                        />
+                    </Routes>
+                </Container>
+
+                {
+                    showSignUpModal &&
+                    <SignUpModal
+                        onDismiss={() => setShowSignUpModal(false)}
+                        onSignUpSuccessful={(user) => {
+                            setLoggedInUser(user);
+                            setShowSignUpModal(false);
+                        }}
+                    />
+                }
+
+                {
+                    showLoginModal &&
+                    <LoginModal
+                        onDismiss={() => setShowLoginModal(false)}
+                        onLoginSuccess={(user) => {
+                            setLoggedInUser(user);
+                            setShowLoginModal(false);
+                        }}
+                    />
+                }
+            </div>
+        </BrowserRouter>
     );
 }
 
